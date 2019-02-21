604fb6e4d8a55c492154074207e956418597dfda
mcconvillej <47699444+mcconvillej@users.noreply.github.com>
2019-02-21 17:43:08 +0000

    Update k237_sweep.py

--- k237 scripts/k237_sweep.py
+++ k237 scripts/k237_sweep.py
@@ -13,9 +13,11 @@ k237.write('Q4,20,30,1,3,1000,60000X') #sweep command
 k237.write('B10,1,X') #bias level command
 k237.write('L.05,0X') #compliance
 k237.write('T1,0,0,0X') #triggers
+
 action = input('Operate? Type Y/N : ')
 if action != 'Y':
     exit()
+    
 k237.write('N1X') #operate
 k237.wait_for_srq()#wait for sweep to finish
 source_data=k237.query_ascii_values('G1,2,2', container=numpy.array) #get data
@@ -41,7 +43,7 @@ par.yaxis.get_label().set_color(p2.get_color())
 leg.texts[1].set_color(p2.get_color())
 
 plt.show()
-#def Sweep(... couldn't be bothered to write a general thing
+
 leave_program = 'N'
 while leave_program != 'Y':
     leave_program = input('Exit program? Type Y/N : ')
